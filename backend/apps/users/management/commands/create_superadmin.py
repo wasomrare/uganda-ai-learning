b1c7fb209925cf28eb5825644bfb93acd4ac71a8
twiinarides <twiinarides@gmail.com>
2026-05-11 17:14:43 +0300

    feat: read admin credentials from env vars ADMIN_USERNAME/ADMIN_PASSWORD/ADMIN_EMAIL

--- backend/apps/users/management/commands/create_superadmin.py
+++ backend/apps/users/management/commands/create_superadmin.py
@@ -1,6 +1,6 @@
 """Management command to create the initial super admin account."""
+import os
 from django.core.management.base import BaseCommand
-from django.conf import settings
 
 
 class Command(BaseCommand):
@@ -12,15 +12,22 @@ class Command(BaseCommand):
             self.stdout.write(self.style.WARNING('Super admin already exists. Skipping.'))
             return
 
-        user = User.objects.create_superuser(
-            username='admin',
-            password='Admin@Uganda2024!',
-            first_name='System',
-            last_name='Administrator',
+        username = os.environ.get('ADMIN_USERNAME', 'admin')
+        password = os.environ.get('ADMIN_PASSWORD', 'Admin@Uganda2024!')
+        email = os.environ.get('ADMIN_EMAIL', 'admin@ugandalearn.com')
+        first_name = os.environ.get('ADMIN_FIRST_NAME', 'System')
+        last_name = os.environ.get('ADMIN_LAST_NAME', 'Administrator')
+
+        User.objects.create_superuser(
+            username=username,
+            email=email,
+            password=password,
+            first_name=first_name,
+            last_name=last_name,
             role='super_admin',
             is_verified=True,
-            force_password_change=True,
+            force_password_change=False,
         )
         self.stdout.write(self.style.SUCCESS(
-            f'Super admin created: username=admin  (please change password immediately)'
+            f'Super admin created: username={username} email={email}'
         ))
